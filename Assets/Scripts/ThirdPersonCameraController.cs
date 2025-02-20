8d5d7f28c169da9efd06c1ad31bfdf0f71643c04
noramknarf <noramknarf@gmail.com>
2025-02-20 18:07:28 +0000

    Idle loop + Basic camera
    Added a character model with an idle loop and copied over the camera behaviours from lab 5.

---
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public float distFromTarget;
+    public bool follow = true;
+    public float displacementY = 1.5f;
+    //Degree to which the camera lags just a little behind the target's motion
+    public float easing = 0.1f;
+    [SerializeField]
+    private GameObject cameraFocusPoint;
+
+    private Vector3 newpos;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (follow)
+        {
+            //+y for above, - z for behind.
+            newpos = target.position - (target.forward * distFromTarget); //+ (target.up * displacementY);
+            newpos.y = target.position.y + displacementY;
+            this.transform.position += (newpos - this.transform.position) * easing;
+            //this.transform.rotation = target.transform.rotation;
+            this.transform.LookAt(cameraFocusPoint.transform);
+        }
+        else
+        {
+            this.transform.LookAt(cameraFocusPoint.transform);
+        }
+    }
+}
