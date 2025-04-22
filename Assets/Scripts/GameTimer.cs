f950bc1e7841fe8ed9cebc949d1c2d77066d5a94
noramknarf <noramknarf@gmail.com>
2025-04-22 15:48:55 +0100

    Added disappearing clock to HUD

--- Assets/Scripts/GameTimer.cs
+++ Assets/Scripts/GameTimer.cs
@@ -7,8 +7,10 @@ using UnityEngine.SceneManagement;
 public class GameTimer : MonoBehaviour
 {
     public Text timerText;
-    
-    public float remainingTime = 20;
+    public Image clockImage;
+
+    public float startingTime = 20.0f;
+    private float remainingTime;
     private int remainingMins;
     private int remainingSeconds;
     private bool loading = false;
@@ -16,15 +18,21 @@ public class GameTimer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        remainingTime = startingTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         remainingTime -= Time.deltaTime;
+        clockImage.fillAmount = remainingTime / startingTime;
+        print("remaining time divided by starting time = " + remainingTime / startingTime);
+        print("clockImage FillAmount = " + clockImage.fillAmount);
         remainingMins = (int)(remainingTime / 60);
         remainingSeconds = (int)(remainingTime % 60);
         timerText.text = string.Format("{0:D2}:{1:D2}", remainingMins, remainingSeconds);
+
+        
         
         if(remainingTime <= 0 && loading == false){
             SceneManager.LoadSceneAsync(2);
